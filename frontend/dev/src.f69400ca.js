e75f9b4870b998ef1a468361689e62ecb3028b13
Dâmaso Júnio <junio.pereirab@gmail.com>
2021-03-14 03:40:27 -0300

    Adding paths structure to the project

--- frontend/dev/src.f69400ca.js
+++ frontend/dev/src.f69400ca.js
@@ -29822,7 +29822,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38687" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36239" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
