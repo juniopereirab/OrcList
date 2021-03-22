9a485d44ea069d80e7293494d5a81b3d7da9149f
Dâmaso Júnio <junio.pereirab@gmail.com>
2021-03-21 23:16:47 -0300

    Burn all the thingsgit add .
    ~

--- frontend/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import { render } from 'react-dom'
-
-const App = () => <h1>Parcel Bundler with React</h1>
-
-render(
-  <App/>,
-  document.getElementById('root')
-)
